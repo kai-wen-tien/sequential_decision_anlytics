49395623e1f80590c4982c15467c6eb3d5c2afbd
Kai-Wen Tien <kai-wen.tien@outlook.com>
2025-06-29 01:12:03 +0800

    Save New Files

---
+++ LLM_SDM/Policy0.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 24 20:57:08 2025
+
+@author: Kai
+"""
+
+class Policy:
+    def __init__(self, imported_energy, market_price, cost):
+        """
+        Initialize policy parameters and state variables.
+        Parameters:
+        - imported_energy (float): Total energy imported so far [kWh]
+        - market_price (float): Current or average market price [€/kWh]
+        - cost (float): Accumulated energy cost [€]
+        """
+        self.imported_energy = imported_energy
+        self.market_price = market_price
+        self.cost = cost
+
+    def take_action(
+        self,
+        state_of_charge: float,      # Current battery charge [kWh]
+        imported_energy: float,      # Current energy import [kWh]
+        market_price: float,         # Current market price [€/kWh]
+        cost: float                  # Current total cost [€]
+    ) -> float:
+        """
+        Determine optimal energy trading action based on current state.
+
+        Returns:
+        float: Action for battery [kWh]
+               Positive → charge
+               Negative → discharge
+        """
+        if market_price < 0.5:
+            action = 10
+        else:
+            action = -5
+        
+        return action
